cb341708a7df4ad2f6969cd96fe0956043294ffc
Zacharie Boris <moucheliboris@gmail.com>
2025-04-29 15:29:24 +0200

    Gestion des erreur de l'OCR

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,27 @@ const nextConfig: NextConfig = {
   eslint: {
     // Ignorer les erreurs ESLint pendant le build
     ignoreDuringBuilds: true
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,POST,PUT,OPTIONS,PATCH,DELETE'
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+          }
+        ]
+      }
+    ]
   }
 };
 
