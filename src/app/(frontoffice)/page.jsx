7336b6157cc11ce7f54d8be663a39e3895810034
Antoine <antoinemaillard06@gmail.com>
2025-11-05 13:54:15 +0100

    fix: front-end style updates

--- src/app/(frontoffice)/page.jsx
+++ src/app/(frontoffice)/page.jsx
@@ -192,18 +192,16 @@ export default function HomePage() {
                   venue={closestEvent.venue}
                   city={closestEvent.city}
                 />
+                {endTime && (
+                  <div className="flex justify-center mb-8 sm:mb-12 md:mb-16 px-4">
+                    <h1 className="text-white text-sm sm:text-base md:text-lg leading-tight block">
+                    Heure limite de participation <span className="text-blue-400 font-bold">{endTime}</span>
+                    </h1>
+                  </div>
+                )}
                 {/* Espace pour compenser le header fixe */}
                 <div className="h-24 sm:h-28 md:h-32 lg:h-36"></div>
               </div>
-
-              {/* Espace flexible pour pousser le contenu vers le bas */}
-              {endTime && (
-                <div className="flex justify-center mb-8 sm:mb-12 md:mb-16 px-4">
-                  <h1 className="text-white text-sm sm:text-base md:text-lg leading-tight block">
-                  Heure limite de participation <span className="text-blue-400 font-bold">{endTime}</span>
-                  </h1>
-                </div>
-              )}
 
               {/* Titre principal */}
               <div className="flex justify-center mb-8 sm:mb-12 md:mb-16 px-4">
